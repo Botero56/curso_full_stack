a35108062e0166ca6dad087474cf072026015130
unknown <boterosamuel3@gmail.com>
2026-06-10 23:22:42 -0500

    primer commit

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+//const express = require('express')
+import express from 'express'
+
+const app = express()
+
+//routing 
+
+app.get('/', (req, res) => {
+    res.send('hola mundo en express')
+})
+
+const port = process.env.PORT || 4000
+//process.env.PORT || => significa que si existe un env utilizara el puerto que este ahi 
+
+app.listen(port, () => {
+    console.log('servidor funcionando en el puerto:', port)
+})
+
