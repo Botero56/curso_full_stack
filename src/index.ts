7c0d96728fa42bd3594c225249866d1905b93e02
unknown <boterosamuel3@gmail.com>
2026-06-10 23:25:40 -0500

    primer commit

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,10 @@ app.get('/', (req, res) => {
     res.send('hola mundo en express')
 })
 
+app.get('/pagina 2', (req, res) => {
+    res.send('hola mundo en express')
+})
+
 const port = process.env.PORT || 4000
 //process.env.PORT || => significa que si existe un env utilizara el puerto que este ahi 
 
